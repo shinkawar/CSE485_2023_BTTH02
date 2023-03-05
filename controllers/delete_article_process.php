5c63748e198b0d400972857bd66298a0a0084049
Thien3521 <121292439+Thien3521@users.noreply.github.com>
2023-03-05 23:15:49 +0700

    Delete delete_article_process.php

--- controllers/delete_article_process.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once 'db.php';
-    $ma_bviet=$_GET['id']; 
-    $xoa_baiviet="DELETE FROM baiviet WHERE ma_bviet=$ma_bviet";
-    mysqli_query($conn,$xoa_baiviet);
-    header("Location: article.php")
-?>
